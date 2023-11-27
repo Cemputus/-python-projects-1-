fdb2aa5cff4c37fdec860e101b243e4de2c824c2
Emmanuel Nsubuga <107301618+Cemputus@users.noreply.github.com>
2023-11-27 16:05:43 +0300

    Group_Anagrams

---
+++ Group_Anagrams.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+def group_anagrams(a):
+    dfdict = defaultdict(list)
+    for i in a:
+        sorted_i = " ".join(sorted(i))
+        dfdict[sorted_i].append(i)
+    return dfdict.values()
+
+
+#let’s test the function by creating a list of words containing anagrams and some other words:
+words = ["tea", "eat", "bat", "ate", "arc", "car"]
+print(group_anagrams(words))
+
+dict_values([['tea', 'eat', 'ate'], ['bat'], ['arc', 'car']])
+
